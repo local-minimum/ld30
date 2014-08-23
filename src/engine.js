0e54295b679bab700d500b2bcc759e26a9d0410b
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-08-23 15:39:24 +0200

    Beginnings of loading data

--- src/engine.js
+++ src/engine.js
@@ -1,1 +1,36 @@
 "use strict";
+
+function Model() {
+
+}
+
+Model.prototype = {
+
+	"setLevel": function(lvlData) {
+
+	}
+}
+
+var MODEL = new Model();
+
+function Engine() {
+	
+}
+
+Engine.prototype = {
+	
+	"loadLevel": function(lvl) {
+		$.getJSON("data/lvl" + lvl + ".json", function(response) {
+			MODEL.setLevel(response);
+		}).error(function() { alert("Could not load level " + lvl); });
+
+	},
+
+	"showMenu": function() {
+
+	}
+
+}
+
+var e = new Engine();
+e.loadLevel(1);

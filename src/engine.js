d536c130a8d5899920676c4f7bd15e6fcba9819f
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-08-25 12:35:21 +0200

    New lvls 9-13

--- src/engine.js
+++ src/engine.js
@@ -574,7 +574,7 @@ function Engine() {
     };
     this.playerOff = this.playerRotations.UP;
     this.curLevel = 0;    
-    this.maxLevel = 10;
+    this.maxLevel = 13;
     this.inMenus = true;
     this.knownActiveLayers = undefined;
     this.offsetX;
